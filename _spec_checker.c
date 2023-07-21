605c74a98d8c091df8f9aed5b8bfe54e1dfe7df6
Azeez Saka <saoconcept1@gmail.com>
2023-07-21 06:06:06 -0700

    add _spec_checker.c file

---
+++ _spec_checker.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* _spec_checker -Checks for valid format specifier
+* and return corresponding function.
+* @specifier: The format specifier to check.
+*
+* Return: Pointer to the corresponding function, or NULL if not found.
+*/
+int (*_spec_checker(const char *specifier))(va_list)
+{
+int idx = 0;
+
+/* Define the func_specifier structure in main.h */
+func_specifier funcArr[] = {
+{"%", _symbol_out},
+{"c", _char_out},
+{"d", _int_out},  /* Use _int_out for %d */
+{"i", _int_out},  /* Use _int_out for %i */
+{"s", _str_out},
+{NULL, NULL}
+};
+
+/* Validate specifier to avoid dereferencing NULL pointers */
+if (specifier == NULL)
+return (NULL);
+
+/* Loop to check for each function */
+while (funcArr[idx].ch != NULL)
+{
+if (*(funcArr[idx].ch) == *specifier)
+{
+return (funcArr[idx].funcList);
+}
+idx++;
+}
+
+return (NULL);
+}
+
